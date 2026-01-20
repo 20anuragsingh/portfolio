221765f3049128919770170ecec941d53e50c358
Anurag Singh <87014944+20anuragsingh@users.noreply.github.com>
2026-01-20 05:57:49 +0000

    commit

--- my-portfolio/src/components/Hero.js
+++ my-portfolio/src/components/Hero.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../../public/images.png'
+import logo from './images/images.png'
 const Hero = () => {
   return (
     <section className="hero-section">
